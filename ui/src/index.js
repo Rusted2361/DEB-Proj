049b8dd3309cd354a944d7a4f099a544c160094c
Hamza <hamzasajid4040@gmail.com>
2023-10-07 22:32:41 +0500

    added new

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//a header comp
+const Header = () =>{
+return <h1>Welcome to my website</h1>
+}
+
+//a button comp
+const Button = () =>{
+return <button>Click me!</button>
+}
+
+//a app comp
+const App = () =>{
+  return(
+    <div>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// const root = ReactDOM.render(<App/>, document.getElementById("root"))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
