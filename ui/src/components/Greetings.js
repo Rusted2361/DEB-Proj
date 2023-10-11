40a516200fdb1d97c86fb8cca1e6f7a5004f9986
Hamza <hamzasajid4040@gmail.com>
2023-10-11 10:38:34 +0500

    integration of API in ui

--- ui/src/components/Greetings.js
+++ ui/src/components/Greetings.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+
+var API_URL = "127.0.0.1:3005";
+
 export default function Greeting() {
+  const [greetings,setgreetings]= useState();
+  const[greetingsList,setgreetingsList]= useState();
+  
+  useEffect(() => {
+    async function getGreetings(){
+      var response = await fetch (`http://${API_URL}/greetings`);
+      let data = await response.json();
+      setgreetingsList(data)
+      setgreetings(data[0])
+    }
+    getGreetings();
+  }, []);
+
+  function changeGreeting(){
+    const randomIndex = Math.floor(Math.random() * greetingsList.length);
+    setgreetings(greetingsList[randomIndex])
+  }
   return (
     <div>
       <p id="greetingTxt" className="font-bold text-2xl py-4">
-        hello world yoy
+        {greetings}
       </p>
       <button
         onclick="changeGreeting()"
-        className="bg-blue-500 hover: bg-blue-700 text-white font-bold py-4 px-6 rounded"
+        className="bg-blue-500 hover: bg-blue-700 text-white font-bold py-4 px-6 rounded" onClick = {changeGreeting}
       >
         change greetings
       </button>
--- ui/src/components/Visitors.js
+++ ui/src/components/Visitors.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+
+var API_URL = "127.0.0.1:3005"
 export default function Visitor() {
+  const [count, setCount] = useState();
+  useEffect (()=>{
+    async function getCount() {
+      var response = await fetch (`http://${API_URL}/visitors`);
+      let data = await response.json();
+      setCount(data);
+    }
+
+    getCount();
+  },[]);
   return (
     <div>
       <p id="visitorCountTxt" className="font-semi-bold text-lg py-4">
-        Visitor count: 78
+        Visitor count: {count}
       </p>
     </div>
   );
