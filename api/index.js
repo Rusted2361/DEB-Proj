5e8207317e8d5df3ae54966e2dcc5f3780576950
Hamza <hamzasajid4040@gmail.com>
2023-10-11 10:38:48 +0500

    created APIs

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import apiroutes from "./api-routes.js";
+
 const listen_port = 3005;
 const app = express()
 
+app.use(cors({origin:"*"}));
 app.use("/", apiroutes);
 
 app.listen(listen_port, () =>{
